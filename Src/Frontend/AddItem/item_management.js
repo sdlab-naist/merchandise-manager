0aa257d0958f9f3e7caee134d5d84d66dc092a63
MSZ006AEUG <zeta@kmc.gr.jp>
2020-01-21 15:29:39 +0900

    ダミーリザルト出るようにした

---
+++ Src/Frontend/AddItem/item_management.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('#submit').click(function() {
+        var name = $('input[name="iname"]').val();
+        var price = $('input[name="iprice"]').val();
+        var cost = $('input[name="icost"]').val();
+        var amount = $('input[name="iamount"]').val();
+        console.log(name, price, cost, amount);
+        
+        $('#result').replaceWith('<div id="result"></div>')
+        $('#result').append("\<table\>\<thead\>\<tr\>\<th\>name\</th\>\<th\>price\</th\>\<th\>cost\</th\>\<th\>amont\</th\>\</tr\>\</thead\>\<tbody\>\<tr\>\<td\>" + 
+        name + "\</td\>\<td\>" +
+        price + "円" + "\</td\>\<td\>" +
+        cost + "円" + "\</td\>\<td\>" +
+        amount + "個" + "\</td\>\</tr\>\</tbody\>\</table\>")
+    });
+});
