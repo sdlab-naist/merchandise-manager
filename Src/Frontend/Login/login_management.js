7618e6477b47cfc11024fa4381ad4eaf078a6d24
Yuki Fukasawa <fukasawa.yuki.fp5@is.naist.jp>
2020-02-17 13:50:53 +0900

    jsファイルを変更した

--- Src/Frontend/Login/login_management.js
+++ Src/Frontend/Login/login_management.js
@@ -1,25 +1,22 @@
 $(function() {
     jQuery.support.cors = true;
-
     $('#submit').click(function() {
         var uid = $('input[name="uid"]').val();
         var pwd = $('input[name="password"]').val();
         console.log(uid, pwd)
-
-        $.post("http://163.221.29.46:13131/login", {
+        jQuery.support.cors = true;
+        $.ajax({
+            type: "POST",
             crossDomain: true,
-            xhrFields: {
-               withCredentials: true
+            xhfFields: {
+                withCredentials: true
             },
-            data: { "Username": uid, "Password": pwd }
-        })
-        .done(function (data, textStatus) {
-            console.log(data, textStatus, "hoge")
-            window.location.href = "http://163.221.29.46:13131/addItemHTML"
-        })
-        .fail(function (data, textStatus) {
-            console.log(data, textStatus)
+            url: "htt://163.221.29.46:13131/login",
+            data: { "Username": uid, "password": pwd},
+            success: function (jsondata) {
+                console.log(jsondata)
+                window.location.href = "http://163.221.29.46:13131/addItemHTML"
+            }
         })
     })
-
 });
